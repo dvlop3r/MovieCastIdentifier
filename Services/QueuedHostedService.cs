09f2038d3bbc462e609366f07f263eebdc5f24da
dvlop3r <sarwan.h.najim@gmail.com>
2022-09-26 02:04:54 +0300

    Add QueuedHostedService to dequeue and execute queued tasks

---
+++ Services/QueuedHostedService.cs
@@ -0,0 +1,48 @@
+namespace MovieCastIdentifier.Services;
+
+public class QueuedHostedService : BackgroundService
+{
+    private readonly IBackgroundTaskQueue _taskQueue;
+    private readonly ILogger<QueuedHostedService> _logger;
+
+    public QueuedHostedService(IBackgroundTaskQueue taskQueue, ILogger<QueueHostedService> logger)
+    {
+        _taskQueue = taskQueue;
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Queue Background Service is starting.");
+
+        stoppingToken.Register(() =>
+            _logger.LogInformation("Queue Background Service is stopping."));
+
+        await BackgroundProcessing(stoppingToken);
+    }
+
+    public async Task BackgroundProcessing(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var workItem = await _taskQueue.DequeueAsync(stoppingToken);
+
+            try
+            {
+                await workItem(stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex,
+                    "Error occurred executing {WorkItem}.", nameof(workItem));
+            }
+        }
+    }
+
+    public override async Task StopAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Queued Hosted Service is stopping.");
+
+        await base.StopAsync(stoppingToken);
+    }
+}
