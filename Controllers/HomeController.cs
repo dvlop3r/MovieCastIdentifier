9f3c8fbae5bded7b12fc49141996cca528775745
dvlop3r <sarwan.h.najim@gmail.com>
2022-09-26 13:04:24 +0300

    Add FileHelper helper to process file streaming

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using MovieCastIdentifier.Atributes;
+using MovieCastIdentifier.Filters;
 using MovieCastIdentifier.Models;
 
 namespace MovieCastIdentifier.Controllers;
