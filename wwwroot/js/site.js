025ac9cc0c80aacd221b6d539fcf9dfcdcf0ac16
dvlop3r <sarwan.h.najim@gmail.com>
2022-09-26 15:06:47 +0300

    Add SignalR engine start and receiving function to site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,49 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", () => {
+    // <snippet_Connection>
+    const connection = new signalR.HubConnectionBuilder()
+        .withUrl("/chathub")
+        .withAutomaticReconnect()
+        .build();
+    // </snippet_Connection>
+
+    // <snippet_ReceiveMessage>
+    connection.on("ReceiveMessage", (user, message) => {
+        $("#message").val(message);
+    });
+    // </snippet_ReceiveMessage>
+
+    // document.getElementById("send").addEventListener("click", async () => {
+    //     const user = document.getElementById("userInput").value;
+    //     const message = document.getElementById("messageInput").value;
+
+    //     // <snippet_Invoke>
+    //     try {
+    //         await connection.invoke("SendMessage", user, message);
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    //     // </snippet_Invoke>
+    // });
+
+
+    async function start() {
+        try {
+            await connection.start();
+            console.log("SignalR Connected.");
+        } catch (err) {
+            console.log(err);
+            setTimeout(start, 5000);
+        }
+    };
+
+    connection.onclose(async () => {
+        await start();
+    });
+
+    // Start the connection.
+    start();
+});
